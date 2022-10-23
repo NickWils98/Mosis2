b9cfc2ebf12f6ec01e11d9e1595096b2090e4610
GauthierLC <Eikenlaan1100049>
2022-10-23 19:03:37 +0200

    Parameter sweep

--- ParameterSweep.py
+++ ParameterSweep.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import scipy
-from scipy import io
+from sklearn.metrics import mean_squared_error
 import os
 
 acceleration_data = pd.read_csv("acceleration.csv")
 deceleration_data = pd.read_csv("deceleration.csv")
 
-working_directory = "/private/var/folders/hf/cmv_674s41s716119nhrhb5h0000gn/T/OpenModelica_gauthierlecompte/OMEdit/ODE/"
-
-
-
 start_S = 150
 end_S = 300
 
-for S in range(start_S, end_S):
-    os.system(f"./ODE -override S={S}")
+for S in range(start_S, end_S+1):
+    os.system(f"./ODE -override S={S},density=988")
+
+    file = pd.read_csv("ODE_res.csv", usecols=['time', 'v'])
+    file.to_csv(f'csv/S{S}.csv', index=False)
+
+#print(mean_squared_error(acceleration_data.values, moto.values))
