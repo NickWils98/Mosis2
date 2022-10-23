cea2ab85bd146497139be0b1ff7d2200039d5c62
NickWils98 <nick.wils@student.uantwerpen.be>
2022-10-23 19:17:06 +0200

    MSE + csvs

--- ParameterSweep.py
+++ ParameterSweep.py
@@ -1,33 +1,28 @@
 import pandas as pd
-import scipy
-from sklearn.metrics import mean_squared_error
 import os
-
-acceleration_data = pd.read_csv("acceleration.csv")
-deceleration_data = pd.read_csv("deceleration.csv")
-
-start_S = 150
-end_S = 300
+from sklearn.metrics import mean_squared_error
 
 MSE_index = 150
 MSE_value = 99999999999999999
 
-for S in range(start_S, end_S+1):
-    os.system(f"./ODE -override S={S},density=988")
+if __name__ == '__main__':
 
-    file = pd.read_csv("ODE_res.csv", usecols=['time', 'v'])
-    file.to_csv(f'csv/S{S}.csv', index=False)
+    acceleration_data = pd.read_csv("acceleration.csv")
+    deceleration_data = pd.read_csv("deceleration.csv")
+    start_S = 150
+    end_S = 300
 
-    S_csv = pd.read_csv(f"csv/S{S}.csv", usecols=['time', 'v'])
-    MSE_temp = mean_squared_error(acceleration_data.values, S_csv.values)
+    for S in range(start_S, end_S+1):
+        # os.system(f"./ODE/ODE -override S={S}")
+        # YachtPlant = pd.read_csv("ODE_res.csv", delimiter=",", skiprows=[2, 3, 84], usecols=["time", "v"])
+        # YachtPlant.to_csv(f"csv/S{S}.csv", index=False)
+    #     YachtPlant = pd.read_csv("ODE_res.csv", delimiter=",")
+    #     print(YachtPlant.head())
+        S_csv = pd.read_csv(f"csv/S{S}.csv")
+        MSE_temp = mean_squared_error(acceleration_data.values, S_csv.values)
 
-    if MSE_temp < MSE_value:
-        MSE_index = S
-        MSE_value = MSE_temp
+        if MSE_temp < MSE_value:
+            MSE_index = S
+            MSE_value = MSE_temp
 
 print(MSE_index)
-
-
-
-
-#print(mean_squared_error(acceleration_data.values, moto.values))
