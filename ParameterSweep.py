eb111c795e2a92fbceb722b82220f96591770edd
GauthierLC <Eikenlaan1100049>
2022-10-23 19:53:38 +0200

    Plot + MSE fix

--- ParameterSweep.py
+++ ParameterSweep.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
 __makecsv__ = False
 #__makecsv__ = True
@@ -15,6 +16,9 @@ if __name__ == '__main__':
     deceleration_data = pd.read_csv("deceleration.csv")
     deceleration_data.v = deceleration_data.v.values[::-1]
 
+    S_list = []
+    MSE_list = []
+
     if __makecsv__:
         for S in range(start_S, end_S+1):
             os.system(f"./ODE/ODE -override S={S}")
@@ -26,9 +30,15 @@ if __name__ == '__main__':
         MSE_ACC = mean_squared_error(acceleration_data.values, S_csv.values)
         MSE_DEC = mean_squared_error(deceleration_data.values, S_csv.values)
 
-        print(f"For file S{S}.csv: MSE = {MSE_ACC + MSE_DEC}")
-        if MSE_ACC+MSE_DEC < MSE_value:
-            MSE_index = S
-            MSE_value = MSE_ACC+MSE_DEC
+        MSE = MSE_ACC+MSE_DEC
+        MSE_list.append(MSE)
+        S_list.append(S)
+        print(f"For file S{S}.csv: MSE = {MSE}")
 
-    print("Best MSE: " + f"{MSE_index}")
+    df = pd.DataFrame(list(zip(S_list, MSE_list)), columns=['S', 'MSE'])
+
+    best_MSE_index = MSE_list.index(min(MSE_list))
+    print("Best MSE: " + f"{S_list[best_MSE_index]}")
+
+    df.plot(x='S', y='MSE', kind='scatter')
+    plt.show()
